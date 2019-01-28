3589f6a34197ed44fde9e7444146788dac50c451
Gonen Shoham <Gonen_Shoham@vfc.com>
2019-01-28 02:21:41 -0800

    Add unit tests for App component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatComponentsModule } from './mat-components/mat-components.module';
+import { TableWithPaginationComponent } from './table-with-pagination/table-with-pagination.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent, TableWithPaginationComponent],
+      imports: [
+        BrowserModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        MatComponentsModule
+      ]
     }).compileComponents();
   }));
 
@@ -16,16 +25,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fleetman'`, () => {
+  it(`should have as title 'FleetMan Inventory'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('fleetman');
+    expect(app.title).toEqual('FleetMan Inventory');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title in a mat-toolbar component', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to fleetman!');
+    expect(compiled.querySelector('mat-toolbar span').textContent).toEqual(
+      'FleetMan Inventory'
+    );
   });
 });
